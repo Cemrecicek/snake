23785c6a0d302e22faf7ade251ca855a562e7f3a
Cemrecicek <cemrecicek0932@gmail.com>
2023-08-02 00:19:46 +0300

    yılana hareket kabiliyeti verildi

--- snake.js
+++ snake.js
@@ -6,20 +6,14 @@ var cols = 21
 var board
 var context
 
-//foksiyonumuz
-
-window.onload = function () {
-    board = document.getElementById("board")
-    board.height = rows * blockSize
-    board.width = cols * blockSize
-    context = board.getContext("2d")
-
-    update()
-}
-//başlangıçta yılan
+//yılanın başlangıcı
 
 var snakeX = blockSize * 10
 var snakeY = blockSize * 10
+
+velocityX=0
+velocityY=0
+
 
 
 //elma
@@ -28,22 +22,67 @@ var elmaX
 var elmaY 
 
 //elmanın koordinatları için fonksiyon
-
-elmaX = Math.floor(Math.random()*cols)*blockSize
-elmaY = Math.floor(Math.random()*rows)*blockSize
-
+function placeFood(){
+    elmaX = Math.floor(Math.random()*cols)*blockSize
+    elmaY = Math.floor(Math.random()*rows)*blockSize
+    
+}
 
 //içerikleri oluşturalım
 
 function update() {
+
+// oyunun bitme durumunu kontrol edelim
+
+   
     context.fillStyle = "black"
     context.fillRect(0, 0, board.width, board.height)
-
-    context.fillStyle = "green"
-    context.fillRect(snakeX, snakeY, blockSize, blockSize)
     
     context.fillStyle = "red"
     context.fillRect(elmaX, elmaY, blockSize, blockSize)
+    
+
+
+    context.fillStyle = "green"
+    snakeX+=velocityX*blockSize
+    snakeY+=velocityY*blockSize
+    context.fillRect(snakeX, snakeY, blockSize, blockSize)
+    
 
     
 }
+
+//foksiyonumuz
+
+window.onload = function () {
+    board = document.getElementById("board")
+    board.height = rows * blockSize
+    board.width = cols * blockSize
+    context = board.getContext("2d")
+
+    placeFood()
+    document.addEventListener("keyup", changeDirection)
+    // update()
+    setInterval(update,1000/10)
+}
+
+function changeDirection(e){
+    if (e.code=="ArrowUp"){
+        velocityX=0
+        velocityY=-1
+    }
+    else if(e.code=="ArrowDown"){
+        velocityX=0
+        velocityY=1
+    }
+    else if(e.code=="ArrowLeft"){
+        velocityX=-1
+        velocityY=0
+    }
+    else if(e.code=="ArrowRight"){
+        velocityX=1
+        velocityY=0
+    }
+}
+
+
