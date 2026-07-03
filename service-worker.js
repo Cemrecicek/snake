1bd1e151ff6fdfabb3670301a6ac05db67260a42
Cemrecicek <cemrecicek0932@gmail.com>
2026-07-03 15:28:37 +0300

    Add PWA support and improve mobile layout

---
+++ service-worker.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = "snake-game-v2";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./snake.js",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedFile) => {
+      return cachedFile || fetch(event.request);
+    })
+  );
+});
--- snake.js
+++ snake.js
@@ -1,152 +1,163 @@
-
 //board
-var blockSize = 25
-var rows = 21
-var cols = 21
-var board
-var context
-
+var blockSize = 25;
+var rows = 21;
+var cols = 21;
+var board;
+var context;
 
 //yılanın başlangıcı
 
-var snakeX = blockSize * 10
-var snakeY = blockSize * 10
+var snakeX = blockSize * 10;
+var snakeY = blockSize * 10;
 
-var velocityX = 0
-var velocityY = 0
+var velocityX = 0;
+var velocityY = 0;
 
-var snakeBody = []
+var snakeBody = [];
 
-var count = 0
-
+var count = 0;
 
 //elma
 
-var elmaX
-var elmaY
+var elmaX;
+var elmaY;
 
 //oyun bitim kontrol fonksiyonu
-var gameOver = false
+var gameOver = false;
 
 //elmanın koordinatları için fonksiyon
 function placeFood() {
-    elmaX = Math.floor(Math.random() * cols) * blockSize
-    elmaY = Math.floor(Math.random() * rows) * blockSize
-
+  elmaX = Math.floor(Math.random() * cols) * blockSize;
+  elmaY = Math.floor(Math.random() * rows) * blockSize;
 }
 
 //içerikleri oluşturalım
 
 function update() {
+  // oyunun bitme durumunu kontrol edelim
+  if (gameOver) {
+    return;
+  }
 
-    // oyunun bitme durumunu kontrol edelim
-    if (gameOver) {
-        return
+  context.fillStyle = "black";
+  context.fillRect(0, 0, board.width, board.height);
+
+  context.fillStyle = "red";
+  context.fillRect(elmaX, elmaY, blockSize, blockSize);
+
+  //Yılanın elmayı yediğini kontrol edelim
+  if (snakeX == elmaX && snakeY == elmaY) {
+    snakeBody.push([elmaX, elmaY]);
+    placeFood();
+    count++;
+    var h2Element = document.querySelector(".skor");
+    h2Element.textContent = "Skor: " + count;
+  }
+
+  for (let i = snakeBody.length - 1; i > 0; i--) {
+    snakeBody[i] = snakeBody[i - 1];
+  }
+
+  if (snakeBody.length) {
+    snakeBody[0] = [snakeX, snakeY];
+  }
+
+  context.fillStyle = "chartreuse";
+  snakeX += velocityX * blockSize;
+  snakeY += velocityY * blockSize;
+  context.fillRect(snakeX, snakeY, blockSize, blockSize);
+
+  for (let i = 0; i < snakeBody.length; i++) {
+    context.fillRect(snakeBody[i][0], snakeBody[i][1], blockSize, blockSize);
+  }
+
+  //oyunun sonlanma durumları
+  if (
+    snakeX < 0 ||
+    snakeX >= cols * blockSize ||
+    snakeY < 0 ||
+    snakeY >= rows * blockSize
+  ) {
+    gameOver = true;
+    alert("Oyununuz bitti");
+  }
+
+  for (let i = 0; i < snakeBody.length; i++) {
+    if (snakeX == snakeBody[i][0] && snakeY == snakeBody[i][1]) {
+      gameOver = true;
+      alert("oyununuz bitti");
     }
-
-
-    context.fillStyle = "black"
-    context.fillRect(0, 0, board.width, board.height)
-
-    context.fillStyle = "red"
-    context.fillRect(elmaX, elmaY, blockSize, blockSize)
-
-
-    //Yılanın elmayı yediğini kontrol edelim
-    if (snakeX == elmaX && snakeY == elmaY) {
-        snakeBody.push([elmaX, elmaY])
-        placeFood()
-        count++
-        var h2Element = document.querySelector('.skor');
-        h2Element.textContent = 'Skor: ' + count;
-
-    }
-
-    for (let i = snakeBody.length - 1; i > 0; i--) {
-        snakeBody[i] = snakeBody[i - 1]
-    }
-
-    if (snakeBody.length) {
-        snakeBody[0] = [snakeX, snakeY]
-    }
-
-    context.fillStyle = "chartreuse"
-    snakeX += velocityX * blockSize
-    snakeY += velocityY * blockSize
-    context.fillRect(snakeX, snakeY, blockSize, blockSize)
-
-    for (let i = 0; i < snakeBody.length; i++) {
-        context.fillRect(snakeBody[i][0], snakeBody[i][1], blockSize, blockSize)
-    }
-
-
-
-    //oyunun sonlanma durumları
-    if (snakeX < 0 || snakeX > cols * blockSize || snakeY < 0 || snakeY > blockSize * rows) {
-        gameOver = true
-        alert("Oyununuz bitti")
-    }
-
-    for (let i = 0; i < snakeBody.length; i++) {
-        if (snakeX == snakeBody[i][0] && snakeY == snakeBody[i][1]) {
-            gameOver = true
-            alert("oyununuz bitti")
-        }
-    }
-
-
-
+  }
 }
 // Oyunu başlatan fonksiyon
 function startGame() {
-    // Oyunun durumunu sıfırla
-    gameOver = false;
-    snakeX = blockSize * 10;
-    snakeY = blockSize * 10;
-    velocityX = 0;
-    velocityY = 0;
-    snakeBody = [];
-    count = 0;
-    placeFood();
+  // Oyunun durumunu sıfırla
+  gameOver = false;
+  snakeX = blockSize * 10;
+  snakeY = blockSize * 10;
+  velocityX = 0;
+  velocityY = 0;
+  snakeBody = [];
+  count = 0;
+  placeFood();
 
-    // Skor alanını sıfırla
-    var h2Element = document.querySelector(".skor");
-    h2Element.textContent = "Skor: " + count;
+  // Skor alanını sıfırla
+  var h2Element = document.querySelector(".skor");
+  h2Element.textContent = "Skor: " + count;
 }
-
 
 //foksiyonumuz
 
 window.onload = function () {
-    board = document.getElementById("board")
-    board.height = rows * blockSize
-    board.width = cols * blockSize
-    context = board.getContext("2d")
+  board = document.getElementById("board");
+  board.height = rows * blockSize;
+  board.width = cols * blockSize;
+  context = board.getContext("2d");
 
-    placeFood()
-    document.addEventListener("keyup", changeDirection)
-    var baslatButton = document.querySelector(".baslat");
-    baslatButton.addEventListener("click", startGame);
-    setInterval(update, 1000 / 10)
-}
+  placeFood();
+  document.addEventListener("keyup", changeDirection);
+  var baslatButton = document.querySelector(".baslat");
+  baslatButton.addEventListener("click", startGame);
+  var controlButtons = document.querySelectorAll(".control-btn");
+
+  controlButtons.forEach(function (button) {
+    button.addEventListener("click", function () {
+      setDirection(button.dataset.direction);
+    });
+  });
+
+  controlButtons.forEach(function (button) {
+    button.addEventListener("click", function () {
+      setDirection(button.dataset.direction);
+    });
+  });
+  setInterval(update, 1000 / 10);
+};
 
 function changeDirection(e) {
-    if (e.code == "ArrowUp" && velocityY != 1) {
-        velocityX = 0
-        velocityY = -1
-    }
-    else if (e.code == "ArrowDown" && velocityY != -1) {
-        velocityX = 0
-        velocityY = 1
-    }
-    else if (e.code == "ArrowLeft" && velocityX != 1) {
-        velocityX = -1
-        velocityY = 0
-    }
-    else if (e.code == "ArrowRight" && velocityX != -1) {
-        velocityX = 1
-        velocityY = 0
-    }
+  if (e.code === "ArrowUp") {
+    setDirection("up");
+  } else if (e.code === "ArrowDown") {
+    setDirection("down");
+  } else if (e.code === "ArrowLeft") {
+    setDirection("left");
+  } else if (e.code === "ArrowRight") {
+    setDirection("right");
+  }
 }
 
-
+function setDirection(direction) {
+  if (direction === "up" && velocityY !== 1) {
+    velocityX = 0;
+    velocityY = -1;
+  } else if (direction === "down" && velocityY !== -1) {
+    velocityX = 0;
+    velocityY = 1;
+  } else if (direction === "left" && velocityX !== 1) {
+    velocityX = -1;
+    velocityY = 0;
+  } else if (direction === "right" && velocityX !== -1) {
+    velocityX = 1;
+    velocityY = 0;
+  }
+}
